594598b4c61073fdaea3bf6dc3e3743b56f9d6c2
Rodney J. Dyer <rjdyer@vcu.edu>
2013-05-29 13:20:16 -0400

    Added dist_cgd and associated tests

---
+++ gstudio/R/dist_cgd.R
@@ -0,0 +1,25 @@
+#' Estimation of conditional genetic distance
+#' 
+#' This function returns a measure of conditional genetic distance based upon
+#'  the Population Graphs approach from Dyer & Nason (2004) and Dyer et al. (2010).
+#' @param stratum The groups among which you are going to estimate genetic distances.
+#' @param x The genetic data, either as a single locus or multilocus (\code{data.frame}) 
+#'  object.  
+#' @return A matrix of conditional genetic distance estimates.
+#' @author Rodney J. Dyer \email{rjdyer@@vcu.edu}
+#' @export
+dist_cgd <- function( x, stratum="Population" ) {
+  
+  if( !is( x, "data.frame") )
+    stop("You need to pass a data.frame to dist_cavalli() to work.")
+  
+  if( !(stratum %in% names(x)))
+    stop("You need to specify the correct stratum for dist_cavalli() to work.")
+
+  require(popgraph)
+  mv <- to_mv( x )
+  graph <- population_graph(x=mv, groups=x[[stratum]] )
+  ret <- to_matrix(lopho, mode = "shortest path")
+  return(ret)
+}
+
