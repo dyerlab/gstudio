e9166ad60471e7dc075b5633e002c870890c4955
roddog <rodney.dyer@gmail.com>
2013-05-30 10:50:26 -0400

    Fixed dist_amova() and its tests

--- gstudio/R/dist_amova.R
+++ gstudio/R/dist_amova.R
@@ -2,14 +2,30 @@
 #' 
 #' This function returns a measure of genetic distance based upon
 #'  the AMOVA distance metric.  
-#' @param idx indices to compare
-#' @param data The genotypes to examine.
-#' @return The AMOVA distance between two individuals.
+#' @param x A \code{data.frame} with \code{locus} columns in it.
+#' @return The AMOVA distance matrix
 #' @author Rodney J. Dyer \email{rjdyer@@vcu.edu}
 #' @export
-dist_amova <- function( idx, data ) {
-  x <- data[idx[1],] 
-  y <- data[idx[2],]
-  ret <- sum( 2*t(x-y) %*% (x-y) )
+dist_amova <- function( x ) {
+  
+  if( is(x,"locus") )
+    x <- data.frame( LOCUS=x )
+  if( !is(x,"data.frame") )
+    stop(paste("The function dist_amova() requires a locus vector or a data frame of locus vectors.  You passed a '",class(x), "' object.",sep=""))
+  
+  N <- dim(x)[1]
+  ret <- matrix(0, ncol=N,nrow=N)
+  data <- to_mv( x, drop.allele=FALSE )
+  
+  for( i in 1:N) {
+    x <- data[i,]
+    for( j in i:N) {
+      if( i != j ) {
+        y <- data[j,]
+        ret[i,j] <- ret[j,i] <- sum( 2*t(x-y) %*% (x-y) )
+      }
+    }
+  }
+  
   return( ret ) 
 }
