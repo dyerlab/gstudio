8ed1de3c40c42bb8bf6fef57b3f09a1293e0b8fb
dyerlab <rjdyer@vcu.edu>
2014-04-11 11:05:59 -0400

    Added Fij() function to gstudio

--- R/fij.R
+++ R/fij.R
@@ -10,16 +10,20 @@
 #' @return A matrix of pairwise values for either the allele or 
 #' 
 Fij <- function( x, allele=NA ){
+  if( !is(x,"locus"))
+    stop("This function takes a vector of locus class objects as an argument.")
   N <- length(x)
   k <- N*(N-1)/2
   ret <- matrix(0,N,N)
   
   freq <- frequencies( x )
-  if( is.na(allele) ) {
+  if( is.na(allele) ) 
     allele <- freq$Allele
-  }
-  else
+  else 
     freq <- freq[ freq$Allele %in% allele, ]
+  
+  if( nrow(freq)==0 )
+    stop("The allele you requested is not in this data set so there is no way to estimate coancestry.")
   
   loci <- to_mv(x,alleles=allele )
   
@@ -30,7 +34,7 @@ Fij <- function( x, allele=NA ){
     for( j in 1:i) {
       if( i!=j) {
         pj <- loci[j,]
-        fij <- mean( ((pi-pbar)*(pj-pbar))/(k*pbar(1-pbar)) + 1/(2*(N-1)) )   
+        fij <- mean( ((pi-pbar)*(pj-pbar))/(k*pbar*(1-pbar)) + 1/(2*(N-1)) )   
         ret[i,j] <- ret[j,i] <- fij
       }
     }
@@ -38,7 +42,7 @@ Fij <- function( x, allele=NA ){
   
   # correct for multi allelic estimators
   if( length(allele) > 1 )
-    ret <- ret / Pe(loci)
+    ret <- ret / Pe(x)
   
   return(ret)
 }
--- inst/tests/test-f_ij.R
+++ inst/tests/test-f_ij.R
@@ -7,18 +7,14 @@ test_that("Inbreeding",{
   AC <- locus( c("A","C") )
   BC <- locus( c("B","C") )
   CC <- locus( c("C","C") )
-  loci <- c(AA,AA,AB,BB,CC,AB,AC,BB,BC,CC)
+  x <- c(AA,AA,AB,BB,CC,AB,AC,BB,BC,CC)
   
-  f <- Fij( loci,allele="X" )
-  expect_that( is(x,"matrix"), is_true())
+  expect_that( f <- Fij( x,allele="X" ), throws_error() )
+  
+  f <- Fij( x,allele="A" )
+  expect_that( is(f,"matrix"), is_true())
   expect_that( dim(x), is_equivalent_to( c(10,10)))
   expect_that( sum(x), is_equivalent_to(0) )
-  
-  f <- Fij( loci,allele="A" )
-  expect_that( is(x,"matrix"), is_true())
-  
-  f <- Fij( loci )
-  expect_that( is(x,"matrix"), is_true())
-  
+  expect_that( f[1,10], is_equivalent_to( 0.1/3) )
 
 })
