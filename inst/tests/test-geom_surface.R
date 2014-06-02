e8ee811990b90bd96c476ce2804585f92dd2d1f2
roddog <rodney.dyer@gmail.com>
2014-06-02 12:16:49 -0400

    cleaning up for gh-pages

--- inst/tests/test-geom_surface.R
+++
@@ -1,8 +0,0 @@
-context("geom_surface.R")
-
-test_that("shortest paths", {
-  surface <- raster::raster( matrix(c(1,3,4,3,1,2,3,4,1,2,2,3,3,2,3,1), nrow=4) )
-  p <- geom_surface( surface )   
-  expect_that( p, is_a("proto") )
-  expect_that( p, is_a("environment") )  
-})
