da5ebee1b8253520121bfaf189534a99d60318ae
dyerlab <rjdyer@vcu.edu>
2016-02-08 06:25:50 -0500

    Added stratum to Fis

--- R/fis.R
+++ R/fis.R
@@ -5,6 +5,8 @@
 #'  
 #' @param x Either a \code{locus} object or a data.frame with locus objects.
 #' @param small.sample.correction Passes this along to He for small sample sizes.
+#' @param stratum An optional term for the estimation of inbreeding from samples of populations 
+#'  (default=NULL)
 #' @return The inbreeding F statistic as a \code{numeric} value or a \code{data.frame}
 #'  if you passed multiple loci to this function.
 #' @export
@@ -12,16 +14,18 @@
 #' @examples
 #' loci <- c( locus( c("A","A") ), locus( c("A","A") ), locus( c("A","B") ) )
 #' Fis( loci )
-Fis <- function( x, small.sample.correction=FALSE ) {
+Fis <- function( x, small.sample.correction=FALSE, stratum=NULL ) {
+  
   if( is(x,"locus") ) {
     ret <- 1.0 - Ho(x) / He(x, small.sample.correction)
     names(ret) <- "Fis"
   }
   else if( is(x,"data.frame")) {
-    cols <- column_class( x, "locus" )
-    ret <- data.frame( Locus=cols, Fis=0)
-    for( i in 1:length(cols) )
-      ret$Fis[i] <- Fis( x[[cols[i]]] )
+    ho <- Ho( x, stratum=stratum )
+    he <- He( x, stratum=stratum )
+    loci <- column_class(x,"locus")
+    Fis <- 1.0 - ho$Ho[ho$Locus == loci]/he$He[he$Locus==loci]
+    ret <- data.frame( Locus=loci, Fis=1.0 - ho$Ho[ho$Locus == loci]/he$He[he$Locus==loci] )
   }
   else
     stop(paste("This function does not know how to handle data of type",class(x)))
