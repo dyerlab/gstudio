ef0db3779ab55cfd49e91ea0c0a6ea3d86654a2e
dyerlab <rjdyer@vcu.edu>
2016-03-17 15:31:05 -0400

    Added ability to ask for subset of loci through genetic_diversity() for A, Ae, and A95.

--- R/a.R
+++ R/a.R
@@ -3,18 +3,25 @@
 #' This function returns the number of unique, non-NA alleles, in a sample.
 #' @param x A set of \code{locus} objects
 #' @param min_freq The minimum frequency of allele to consider (default=0)
+#' @param loci A list of loci to use (if a \code{data.frame}), if not given
+#'  then all will be considered.
+#' @param ... Ignored
 #' @return The number of alleles that have \code{min_freq} frequency.
 #' @export
 #' @examples
 #' loci <- c( locus(1:2), locus(c(1,1)), locus(c(2,2)), locus(2:3) )
 #' A(loci)
 #' A(loci, min_freq=0.13)
-A <- function(x, min_freq=0 ){
+A <- function(x, min_freq=0, loci = NULL, ...){
 
   if( is( x, "data.frame" ) ){
-    locus_names <- column_class(x,"locus")
-    if( length(locus_names)==0 )
-      stop("You must pass some loci to the A() function.")
+    if( is.null(loci))
+      locus_names <- column_class(x,"locus")
+    else
+      locus_names <- loci
+
+    if( length(locus_names)==0 || !all(locus_names %in% names(x)) )
+      stop("You must pass some data that has locus object to the A() function.")
     K <- length(locus_names)
     ret <- data.frame( Locus=locus_names, A=0 )
     for( i in 1:K){
