1e1a9cc7acc560a73517e696879fd426ab5eec56
dyerlab <rjdyer@vcu.edu>
2019-04-16 14:02:03 -0400

    Adding population_graph() function to gstudio to merge subsampling loci and quick making of graphs from genotypes.

---
+++ R/population_graph.R
@@ -0,0 +1,23 @@
+#' Convience function to create population graph object from genotypes.
+#' 
+#' This function is a convience function that wraps together the subset_loci() (potentially)
+#'   function with the popgraph::popgraph() function (and translations from genotypes to 
+#'   multivariate data).
+#' @param x A \code{data.frame} with \code{locus} objects
+#' @param stratum The stratum column to use as the Node designation (default="Population").
+#' @param numLoci If not \code{NULL} then how many randomly selected loci (fewer than the total)
+#'   to use in the estimation.
+#' @param ... Other parameters passed to the \code{popgraph::popgraph()} function.
+#' @export
+
+population_graph <- function( x, stratum="Population", numLoci=NULL, ...) {
+  if( !is(x,"data.frame")){
+    stop("Must pass a data.frame object to this function.")
+  }
+  
+  
+  data <- to_mv( ifelse( is.null(numLoci), x, subsample_loci( x, numLoci ) ) )
+  groups <- as.factor( x[[stratum]] )
+  
+  return( popgraph::popgraph(data,groups,...))
+}
