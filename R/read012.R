5b32115290c28391b8947c3a939ca055e191b4db
dyerlab <dyerlab@icloud.com>
2023-09-18 14:19:36 -0400

    reading in 012 format started

---
+++ R/read012.R
@@ -0,0 +1,15 @@
+#' Imports from 012 files
+#' 
+#' This function reads in data that is stored in 012 format into gstudio
+#'  and 
+#' @param fileName The name of the base file with the 012 encoding. This is
+#'  typically something like 'out.012'.  This function will infer that there 
+#'  are 2 more files in the same location, one ending in `.indiv` that will be 
+#'  the names of the locales where the individuals were sampled, and `.pos` 
+#'  which will indicate the contig onto which the 
+#' @return A data.frame with formatted genotypes
+#' @author Rodney J. Dyer \email{rjdyer@@vcu.edu}
+
+from012 <- function( fileName ) {
+  return( NULL )
+}
